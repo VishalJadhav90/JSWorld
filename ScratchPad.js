b25a06c6d084aedc2268a0d61c89e12abcdaf5ed
VishalJadhav90 <jadhav.vsh@gmail.com>
2019-11-25 21:17:19 +0530

    First commit into JavaScript world !

---
+++ ScratchPad.js
@@ -0,0 +1,32 @@
+"use strict";
+
+/*
+let marks = 20;
+alert(marks);
+let fullname = "vishal jadhav";
+alert(fullname);
+let name = "vj";
+alert(name);
+
+const pi = 3.14;
+pi = 22/7
+alert(pi);
+
+*/
+
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
+
+let color = COLOR_ORANGE;
+alert(color);
+
+let admin, name;
+name = "John";
+admin = name;
+
+alert(admin);
+
+const PLANET = 'earth';
+let currentVisitor;
