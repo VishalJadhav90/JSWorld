d9c5333b8bd86d8dce3080553e61b92a98168111
VishalJadhav90 <jadhav.vsh@gmail.com>
2019-11-25 21:55:42 +0530

    Conversion in JavaScript

---
+++ TypeConversions.js
@@ -0,0 +1,18 @@
+"use strict";
+
+//string conversion
+let variable = true;
+
+console.log(typeof variable);
+
+variable = String(true);
+console.log(typeof variable);
+
+//number conversion
+let num1 = "10";
+let num2 = "2";
+console.log(num1 / num2);
+
+let number = Number(num1)
+console.log(number)
+
