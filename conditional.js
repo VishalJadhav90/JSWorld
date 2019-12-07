7ecd14e19219050c26c4419acf214ceb3066061c
VishalJadhav90 <jadhav.vsh@gmail.com>
2019-12-07 08:59:58 +0530

    examples with conditional statements

---
+++ conditional.js
@@ -0,0 +1,8 @@
+"use strict";
+let a = "a" / "b";
+if (! a) {
+    alert("falsy");
+}
+
+let e = ("a" == "b");
+console.log(+e);
--- prompt.js
+++ prompt.js
@@ -3,3 +3,9 @@
 let value = 0;
 value = prompt("your input", 0);
 alert(value);
+
+if (value==10) {
+    alert("seems you have entered " + value);
+}
+
+
